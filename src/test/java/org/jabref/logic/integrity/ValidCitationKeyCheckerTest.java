f36d10698fbfdf63a457c8206cecac5b7d50e241
Sebastian Hafström <hafstrom.sebastian@gamil.com>
2020-10-06 16:02:07 +0200

    Why leave out the unused import

--- src/test/java/org/jabref/logic/integrity/ValidCitationKeyCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/ValidCitationKeyCheckerTest.java
@@ -6,7 +6,6 @@ import org.jabref.logic.l10n.Localization;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ValidCitationKeyCheckerTest {
 
