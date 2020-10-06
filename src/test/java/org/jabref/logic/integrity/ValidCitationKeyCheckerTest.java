8ca04a4b8e4b3839395ac2a6ea0e1b9813cfb6aa
Sebastian Hafström <hafstrom.sebastian@gamil.com>
2020-10-06 16:01:06 +0200

    #5 Fully covering the ValidCitationKeyChecker class

--- src/test/java/org/jabref/logic/integrity/ValidCitationKeyCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/ValidCitationKeyCheckerTest.java
@@ -2,6 +2,7 @@ package org.jabref.logic.integrity;
 
 import java.util.Optional;
 
+import org.jabref.logic.l10n.Localization;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,11 +14,27 @@ public class ValidCitationKeyCheckerTest {
 
     @Test
     void citationKeyIsEmptyString() {
-        assertNotEquals(Optional.empty(), checker.checkValue(""));
+        // assertNotEquals(Optional.empty(), checker.checkValue(""));
+        assertEquals(Optional.of(Localization.lang("empty citation key")), checker.checkValue(""));
     }
 
     @Test
     void citationKeyIsNull() {
-        assertNotEquals(Optional.empty(), checker.checkValue(null));
+        // assertNotEquals(Optional.empty(), checker.checkValue(null));
+        assertEquals(Optional.of(Localization.lang("empty citation key")), checker.checkValue(null));
     }
+
+    @Test
+    void citationKeyIsValid() {
+        // assertNotEquals(Optional.empty(), checker.checkValue(null));
+        assertEquals(Optional.empty(), checker.checkValue("Slominski2019"));
+    }
+
+    @Test
+    void citationKeyIsNotValid() {
+        // assertNotEquals(Optional.empty(), checker.checkValue(null));
+        assertEquals(Optional.of(Localization.lang("Invalid citation key")), checker.checkValue("Slominski2019ÖÖ%%"));
+    }
+
+
 }
