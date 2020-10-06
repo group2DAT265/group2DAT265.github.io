6595befbb734bdc05665bf4c2114b78e559c108f
Sebastian Hafström <hafstrom.sebastian@gamil.com>
2020-10-06 20:39:35 +0200

    #5 [CodeCod] Fully covered MetaDataDiff with tests

--- src/test/java/org/jabref/logic/bibtex/comparator/MetaDataDiffTest.java
+++ src/test/java/org/jabref/logic/bibtex/comparator/MetaDataDiffTest.java
@@ -1,23 +1,36 @@
 package org.jabref.logic.bibtex.comparator;
 
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.jabref.logic.citationkeypattern.AbstractCitationKeyPattern;
+import org.jabref.logic.citationkeypattern.DatabaseCitationKeyPattern;
+import org.jabref.logic.citationkeypattern.GlobalCitationKeyPattern;
+import org.jabref.logic.cleanup.FieldFormatterCleanup;
+import org.jabref.logic.cleanup.FieldFormatterCleanups;
+import org.jabref.logic.formatter.casechanger.LowerCaseFormatter;
 import org.jabref.logic.l10n.Localization;
+import org.jabref.model.database.BibDatabaseMode;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.entry.types.StandardEntryType;
 import org.jabref.model.groups.AbstractGroup;
 import org.jabref.model.groups.GroupHierarchyType;
 import org.jabref.model.groups.GroupTreeNode;
 import org.jabref.model.metadata.ContentSelector;
 import org.jabref.model.metadata.MetaData;
+import org.jabref.model.metadata.SaveOrderConfig;
 import org.jabref.preferences.JabRefPreferences;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
 
 public class MetaDataDiffTest {
 
@@ -103,6 +116,150 @@ public class MetaDataDiffTest {
         
         List<String> expected = new ArrayList<>();
         expected.add(Localization.lang("Modified groups tree"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentEncoding() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        originalMetaData.setEncoding(Charset.forName("UTF-8"));
+        newMetaData.setEncoding(Charset.forName("UTF-16"));
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("Library encoding"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentSaveOrderConfigurations() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        SaveOrderConfig saveOrderConfig = new SaveOrderConfig();
+        SaveOrderConfig saveOrderConfigOther = new SaveOrderConfig();
+        saveOrderConfigOther.setSaveInSpecifiedOrder();
+
+
+        originalMetaData.setSaveOrderConfig(saveOrderConfig);
+        newMetaData.setSaveOrderConfig(saveOrderConfigOther);
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("Save sort order"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentCiteKeyPatterns() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        AbstractCitationKeyPattern pattern = new DatabaseCitationKeyPattern(mock(GlobalCitationKeyPattern.class));
+        pattern.setDefaultValue("test");
+        pattern.addCitationKeyPattern(StandardEntryType.ARTICLE, "articleTest");
+        originalMetaData.setCiteKeyPattern(pattern);
+
+        AbstractCitationKeyPattern patternNew = new DatabaseCitationKeyPattern(mock(GlobalCitationKeyPattern.class));
+        patternNew.setDefaultValue("testing");
+        patternNew.addCitationKeyPattern(StandardEntryType.ARTICLE, "articleTest");
+        newMetaData.setCiteKeyPattern(patternNew);
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("Key patterns"));
+        expected.add(Localization.lang("Default pattern"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentUserFileDirectories() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        originalMetaData.setUserFileDirectory("user", "path");
+        newMetaData.setUserFileDirectory("userNew", "pathNew");
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("User-specific file directory"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentLatexFileDirectories() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        originalMetaData.setLatexFileDirectory("user", Paths.get("/tmp/foo"));
+        newMetaData.setLatexFileDirectory("user", Paths.get("/tmp/foo/bar"));
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("LaTeX file directory"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentSaveActions() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        FieldFormatterCleanups saveActions = new FieldFormatterCleanups(true, Collections.singletonList(new FieldFormatterCleanup(StandardField.TITLE, new LowerCaseFormatter())));
+        FieldFormatterCleanups saveActionsNew = new FieldFormatterCleanups(true, Collections.singletonList(new FieldFormatterCleanup(StandardField.AUTHOR, new LowerCaseFormatter())));
+
+        originalMetaData.setSaveActions(saveActions);
+        newMetaData.setSaveActions(saveActionsNew);
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("Save actions"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentModes() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        originalMetaData.setMode(BibDatabaseMode.BIBTEX);
+        newMetaData.setMode(BibDatabaseMode.BIBLATEX);
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("Library mode"));
+
+        assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
+    }
+
+    @Test
+    public void compareTwoMetaDataWithDifferentDefaultFileDirectories() throws Exception {
+        MetaData originalMetaData = new MetaData();
+        MetaData newMetaData = new MetaData();
+
+        originalMetaData.setDefaultFileDirectory("tmp/foo");
+        newMetaData.setDefaultFileDirectory("tmp/foo/bar");
+
+        MetaDataDiff metaDataDiff = MetaDataDiff.compare(originalMetaData, newMetaData).get();
+        
+        List<String> expected = new ArrayList<>();
+        expected.add(Localization.lang("General file directory"));
 
         assertLinesMatch(expected, metaDataDiff.getDifferences(prefs));
     }
