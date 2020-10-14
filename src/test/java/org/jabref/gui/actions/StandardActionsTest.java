5d66673834c9ca86614eafcc13ce89725a208fd4
Sebastian Hafström <hafstrom.sebastian@gmail.com>
2020-10-14 13:31:21 +0200

    #5 Added tests to achieve 100% coverage of StandardActions

--- src/test/java/org/jabref/gui/actions/StandardActionsTest.java
+++ src/test/java/org/jabref/gui/actions/StandardActionsTest.java
@@ -1,5 +1,13 @@
 package org.jabref.gui.actions;
 
+import java.util.Optional;
+
+import org.jabref.gui.icon.IconTheme;
+import org.jabref.gui.icon.JabRefIcon;
+import org.jabref.gui.keyboard.KeyBinding;
+import org.jabref.logic.l10n.Localization;
+
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +19,28 @@ public class StandardActionsTest {
 
     @BeforeEach
     void setUp() {
-        action = StandardActions.COPY_CITATION_TEXT;
+        action = StandardActions.SAVE_ALL;
     }
 
     @Test
     void verifyGetText() {
-        assertEquals("Text", action.getText());
+        assertEquals(Localization.lang("Save all"), action.getText());
+    }
 
+    @Test
+    void verifyGetDescription() {
+    	assertEquals(Localization.lang("Save all open libraries"), action.getDescription());
+    }
+
+    @Test
+    void verifyGetKeyBinding() {
+        Optional<KeyBinding> keyBinding = Optional.of(KeyBinding.SAVE_ALL);
+        assertEquals(keyBinding, action.getKeyBinding());
+    }
+
+    @Test
+    void verifyGetIcon() {
+        Optional<JabRefIcon> icon = Optional.of(IconTheme.JabRefIcons.SAVE_ALL);
+        assertEquals(icon, action.getIcon());
     }
 }
