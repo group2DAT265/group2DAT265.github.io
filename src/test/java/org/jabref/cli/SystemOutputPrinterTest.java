ff33db7d0a8a790275bceebc425a67836e3e64c2
Naren Hari Krishnan <naren.hari.krishnan@ericsson.com>
2020-10-14 22:03:05 +0200

    #5[CodeCov]Add more test to cli

--- src/test/java/org/jabref/cli/SystemOutputPrinterTest.java
+++ src/test/java/org/jabref/cli/SystemOutputPrinterTest.java
@@ -1,22 +1,42 @@
 package org.jabref.cli;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.mockito.Mockito.*;
 
 class SystemOutputPrinterTest {
 
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
     @Test
     void setStatusTest() {
         SystemOutputPrinter sop = mock(SystemOutputPrinter.class);
-        sop.setStatus("");
-        verify(sop, times(1)).setStatus("");
+        verify(sop, times(1)).setStatus("message");
+        assertEquals("message", outputStreamCaptor.toString().trim());
     }
 
     @Test
     void showMessageTest() {
         SystemOutputPrinter sop = mock(SystemOutputPrinter.class);
-        sop.showMessage("", "", 1);
-        verify(sop, times(1)).showMessage("", "", 1);
+        verify(sop, times(1)).showMessage("message");
+        assertEquals("message", outputStreamCaptor.toString().trim());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
     }
 }
