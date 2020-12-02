99d0d83a8cd365a1e789b29ef539e8fc571d3fd2
Sebastian Hafström <hafstrom.sebastian@gamil.com>
2020-12-02 09:50:25 +0100

    Testing the GUI

---
+++ src/test/java/org/jabref/gui/JabRefLauncherTest.java
@@ -0,0 +1,15 @@
+package org.jabref.gui;
+
+import org.junit.jupiter.api.Test;
+
+public class JabRefLauncherTest {
+
+    @Test
+    public void test()
+    {
+        JabRefLauncher.main(new String[]{
+        });
+
+    }
+
+}
